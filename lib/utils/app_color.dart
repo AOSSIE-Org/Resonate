f5b339187719a76cc323f4361e4730dca97e7ec4
Akshat <110121009@nitt.edu>
2023-03-22 16:03:12 +0530

    added the static class containing all App colors

---
+++ lib/utils/app_color.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/painting.dart';
+
+///static class contains all App colors
+class AppColor {
+  static const LightBrown = Color(0xfff1efe5);
+  static const LightGreen = Color(0xffE8FCD9);
+  static const LightGrey = Color(0xfff2f2f2);
+  static const AccentRed = Color(0xffDA6864);
+  static const AccentGreen = Color(0xff55AB67);
+  static const AccentBrown = Color(0xffE6E2D6);
+  static const AccentBlue = Color(0xff5B75A6);
+  static const AccentGrey = Color(0xff807970);
+  static const DarkBrown = Color(0xff918E81);
+  static const SelectedItemGrey = Color(0xffCCCFDC);
+  static const SelectedItemBorderGrey = Color(0xffC5C5CF);
+}
