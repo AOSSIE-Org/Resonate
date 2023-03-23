bf61fbb1763407287acaeebcf02c5b4241088e0d
chandansgowda <chandansuresh007@gmail.com>
2023-03-23 23:06:17 +0530

    feat: added GetX and MVC folder structure

---
+++ lib/models/example.dart
@@ -0,0 +1,6 @@
+//TODO: Delete this file after adding any file to this directory
+
+class Example{
+  int a = 1;
+}
+
---
+++ lib/views/screens/example_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+//TODO: Delete this file after adding any file to this directory
+class ExampleScreen extends StatelessWidget {
+  const ExampleScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
