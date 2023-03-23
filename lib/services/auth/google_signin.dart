4e325c1d8500777816662f6efc3eee82c11f81bb
chandansgowda <chandansuresh007@gmail.com>
2023-03-23 23:58:48 +0530

    feat: support for google sign in on iOS

--- lib/services/auth/google_signin.dart
+++ lib/services/auth/google_signin.dart
@@ -1,14 +1,16 @@
-
-
+import 'dart:io' show Platform;
 import 'package:google_sign_in/google_sign_in.dart';
 
-
-class GoogleSignInApi{
+class GoogleSignInApi {
+  static String _clientId =
+      "375993303437-tsvpf8s7gbs653429utb1p1hgpto4gbv.apps.googleusercontent.com";
+  static GoogleSignIn _googleSignIn =
+      (Platform.isAndroid) ? GoogleSignIn() : GoogleSignIn(clientId: _clientId);
 
   static Future login() async {
     try {
-      GoogleSignIn googleSignIn = GoogleSignIn();
-      var user = await googleSignIn.signIn();
+      var user = await _googleSignIn.signIn();
+      print(user?.displayName!);
       return user;
     } catch (error) {
       print(error);
@@ -16,8 +18,6 @@ class GoogleSignInApi{
   }
 
   static void logout() async {
-    GoogleSignIn googleSignIn = GoogleSignIn();
-    await googleSignIn.signOut();
+    await _googleSignIn.signOut();
   }
-
 }
