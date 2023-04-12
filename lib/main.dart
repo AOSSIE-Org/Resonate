5de4c6c8e2fdbb1ff07494b955a91716e686e369
chandansgowda <chandansuresh007@gmail.com>
2023-04-12 21:49:54 +0530

    feat: configured firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:resonate/bindings/authentication_binding.dart';
 import 'package:resonate/routes/app_pages.dart';
 import 'package:resonate/views/screens/login_screen.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
