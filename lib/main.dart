b21d2037d16f6f200c7343b9d4d7cd16e4eebb4d
chandansgowda <chandansuresh007@gmail.com>
2023-04-14 12:48:21 +0530

    fix: resolved iOS app crash on launch

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,13 @@ import 'package:resonate/bindings/authentication_binding.dart';
 import 'package:resonate/routes/app_pages.dart';
 import 'package:resonate/views/screens/login_screen.dart';
 
+import 'firebase_options.dart';
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
