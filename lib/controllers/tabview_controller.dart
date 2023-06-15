d9b61f3ed7cceb226553ab4b8607a5169372e599
chandansgowda <chandansuresh007@gmail.com>
2023-06-15 14:01:44 +0530

    refactor: room screen to a separate file

--- lib/controllers/tabview_controller.dart
+++ lib/controllers/tabview_controller.dart
@@ -5,6 +5,7 @@ import 'package:resonate/utils/colors.dart';
 import 'package:resonate/views/widgets/participant_block.dart';
 
 import '../models/participant.dart';
+import '../views/screens/room_screen.dart';
 
 class TabViewController extends GetxController {
   RxInt _selectedIndex = 0.obs;
@@ -15,141 +16,7 @@ class TabViewController extends GetxController {
     showModalBottomSheet(
         context: Get.context!,
         builder: (ctx) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Container(
-                    margin: EdgeInsets.symmetric(vertical: 15),
-                    height: 7,
-                    width: 80,
-                    decoration: BoxDecoration(
-                        color: Colors.amber.withOpacity(0.5),
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                  ),
-                ),
-                SizedBox(
-                  height: Get.height * 0.015,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      "For the love of open source ♥",
-                      style: TextStyle(fontSize: 20, color: Colors.amber),
-                    ),
-                    Spacer(),
-                    FaIcon(
-                      FontAwesomeIcons.ellipsis,
-                      color: Colors.amber,
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: Get.height * 0.001,
-                ),
-                Text("Open Source · Flutter",
-                    style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w100,
-                        color: Colors.white)),
-                SizedBox(
-                  height: Get.height * 0.008,
-                ),
-                Text(
-                  "This is a simple description of the room. You can feature a participant or talk about an event.",
-                  style: TextStyle(color: Colors.white70),
-                ),
-                SizedBox(
-                  height: Get.height * 0.008,
-                ),
-                Divider(
-                  thickness: 2,
-                ),
-                Expanded(
-                  child: Container(
-                    padding: EdgeInsets.symmetric(vertical: 5),
-                    child: GridView.builder(
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 3,
-                          crossAxisSpacing: 20.0,
-                          mainAxisSpacing: 5.0,
-                          childAspectRatio: 2.5 / 3,
-                        ),
-                        itemCount: 15,
-                        itemBuilder: (ctx, index) {
-                          // TODO: Sample Participant - replace it
-                          Participant participantObj = Participant(
-                              email: "c@c.com",
-                              name: "Chandan",
-                              dpUrl:
-                                  "https://avatars.githubusercontent.com/u/41890434?v=4",
-                              isAdmin: index==0,
-                              isMicOn: index%2==0,
-                              isModerator: false,
-                              isSpeaker: index<6,
-                              isSpeaking: false);
-                          return ParticipantBlock(participant: participantObj);
-                        }),
-                  ),
-                ),
-                Divider(
-                  thickness: 2,
-                ),
-                SafeArea(
-                  child: Container(
-                    height: Get.height * 0.075,
-                    child: Column(
-                      children: [
-                        SizedBox(height: Get.height*0.01,),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Container(
-                              height: 40,
-                              width: 125,
-                              decoration: BoxDecoration(
-                                  gradient: AppColor.gradientBg,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(20))),
-                              child: Center(
-                                  child: Text(
-                                "Leave Room",
-                                style: TextStyle(color: Colors.black87),
-                              )),
-                            ),
-                            FloatingActionButton(
-                              onPressed: () {},
-                              backgroundColor: Colors.green,
-                              child: Icon(
-                                Icons.mic,
-                                color: Colors.black54,
-                              ),
-                            ),
-                            Container(
-                              height: 40,
-                              width: 125,
-                              decoration: BoxDecoration(
-                                  gradient: AppColor.gradientBg,
-                                  borderRadius:
-                                  BorderRadius.all(Radius.circular(20))),
-                              child: Center(
-                                  child: Text(
-                                    "26+ Active",
-                                    style: TextStyle(color: Colors.black87),
-                                  )),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
-          );
+          return RoomScreen();
         },
         useSafeArea: true,
         shape: RoundedRectangleBorder(
@@ -161,3 +28,5 @@ class TabViewController extends GetxController {
         isDismissible: false);
   }
 }
+
+
