e0cad4194ab88a878a2da4645548dc5b89d601f2
Aarush Acharya <92685647+Aarush-Acharya@users.noreply.github.com>
2023-07-02 11:43:53 +0530

    feat: tunnelled appwrite endpoint integrated

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,9 +2,9 @@
 
 //TODO: Update Appwrite Project Variables
 const String APPWRITE_PROJECT_ID = "649e58b4b02a9344d10b";
-const String APPWRITE_ENDPOINT = "http://192.168.29.57:85/v1";
+const String APPWRITE_ENDPOINT = "https://ea3d-2405-201-4015-c089-2500-ec89-a3c1-c93d.ngrok-free.app/v1";
 const String DataBaseID = "649e7301d45297f2f52f";
 const String CollectionID = "649e7313b80eb96ebf3f";
 const String userProfileImagePlaceholderUrl =
-    "http://192.168.29.57:85/v1/storage/buckets/649e5b51832289abe07a/files/649e6005937111e885fc/view?project=649e58b4b02a9344d10b";
+    "https://ea3d-2405-201-4015-c089-2500-ec89-a3c1-c93d.ngrok-free.app/v1/storage/buckets/649e5b51832289abe07a/files/649e6005937111e885fc/view?project=649e58b4b02a9344d10b";
 const String userProfileImageBucketId = "649e5b51832289abe07a";
