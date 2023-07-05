0526ee519a326853e2433f79e745f613e349412d
chandansgowda <chandansuresh007@gmail.com>
2023-07-05 14:58:17 +0530

    change: store user data in users database
    Previously it was being stored in user preferences. This cannot be accessed by other users.

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -5,6 +5,7 @@ const String appwriteProjectId = "64a12ff22a44f02f0545";
 const String appwriteEndpoint = "https://cloud.appwrite.io/v1";
 
 const String userDatabaseID = "64a1319104a149e16f5c";
+const String usersCollectionID = "64a52f0a6c41ded09def";
 const String usernameCollectionID = "64a131980b5388c2a0af";
 const String userProfileImagePlaceholderUrl =
     "https://cloud.appwrite.io/v1/storage/buckets/64a13095a4c87fd78bc6/files/64a130ec38af1a3f0d61/view?project=64a12ff22a44f02f0545";
