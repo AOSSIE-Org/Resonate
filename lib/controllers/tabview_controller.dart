61a183c3e83f69aefb796829d84405cedcc4961b
chandansgowda <chandansuresh007@gmail.com>
2023-08-05 07:40:07 +0530

    feat: implemented room link intent flow

--- lib/controllers/tabview_controller.dart
+++ lib/controllers/tabview_controller.dart
@@ -4,8 +4,11 @@ import 'dart:developer';
 import 'package:app_links/app_links.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:resonate/controllers/auth_state_controller.dart';
+import 'package:resonate/controllers/rooms_controller.dart';
 import 'package:resonate/models/appwrite_room.dart';
 import 'package:resonate/utils/colors.dart';
+import 'package:resonate/views/widgets/room_tile.dart';
 
 import '../views/screens/room_screen.dart';
 
@@ -17,12 +20,14 @@ class TabViewController extends GetxController {
   late AppLinks _appLinks;
   StreamSubscription<Uri>? _linkSubscription;
 
+  @override
   void onInit() {
     super.onInit();
     initAppLinks();
   }
 
-  void onDispose(){
+  @override
+  void onDispose() {
     _linkSubscription?.cancel();
     super.dispose();
   }
@@ -44,15 +49,24 @@ class TabViewController extends GetxController {
     });
   }
 
-  void openAppLink(Uri uri) {
-    log("Open Link Called");
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      Get.defaultDialog(title: uri.pathSegments.last);
-    });
+  void openAppLink(Uri uri) async {
+    try {
+      String roomId = uri.pathSegments.last;
+      bool isUserLoggedIn = await Get.find<AuthStateController>().getLoginState;
+      if (isUserLoggedIn) {
+        AppwriteRoom appwriteRoom = await Get.find<RoomsController>().getRoomById(roomId);
+        Get.defaultDialog(
+            title: "Join Room",
+            titleStyle: const TextStyle(color: Colors.amber, fontSize: 25),
+            content: Column(
+              children: [RoomTile(room: appwriteRoom)],
+            ),
+        backgroundColor: AppColor.bgBlackColor);
+      }
+    } catch (e) {
+      log("Open App Link ERROR : ${e.toString()}");
+    }
   }
-
-
 
   void openRoomSheet(AppwriteRoom room) {
     showModalBottomSheet(
@@ -70,5 +84,3 @@ class TabViewController extends GetxController {
         isDismissible: false);
   }
 }
-
-
