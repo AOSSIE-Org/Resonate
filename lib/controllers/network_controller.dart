c212e4b28123ba2af5e59738a21591bf2b73e456
chandansgowda <chandansuresh007@gmail.com>
2023-08-06 08:23:54 +0530

    change: network check package

--- lib/controllers/network_controller.dart
+++ lib/controllers/network_controller.dart
@@ -1,30 +1,22 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
-import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+import 'package:observe_internet_connectivity/observe_internet_connectivity.dart';
 import 'package:resonate/views/widgets/no_connection_dialog.dart';
 
-import '../utils/colors.dart';
-
 class NetworkController extends GetxController {
-  final Connectivity _connectivity = Connectivity();
+  final InternetConnectivity _connectivity = InternetConnectivity();
 
   @override
   void onInit() {
     super.onInit();
-    _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+    _connectivity.observeInternetConnection.listen(_updateConnectionStatus);
   }
 
-  void _updateConnectionStatus(ConnectivityResult connectivityResult) async {
-    if (connectivityResult == ConnectivityResult.none) {
-      Get.dialog(
-        const NoConnectionDialog(),
-          barrierDismissible: false);
+  void _updateConnectionStatus(bool hasInternetAccess) async {
+    if (!hasInternetAccess) {
+      Get.dialog(const NoConnectionDialog(), barrierDismissible: false);
     } else {
-      bool isDeviceConnected = await InternetConnectionChecker().hasConnection;
-      if (isDeviceConnected && Get.isDialogOpen!) {
+      if (Get.isDialogOpen!) {
         Get.back();
       }
     }
