a38d9bc24902edb2403228750a0b5544fff3dbe9
chandansgowda <chandansuresh007@gmail.com>
2023-08-06 08:12:42 +0530

    refactor: no connection dialog

--- lib/controllers/network_controller.dart
+++ lib/controllers/network_controller.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:resonate/views/widgets/no_connection_dialog.dart';
 
 import '../utils/colors.dart';
 
@@ -19,41 +20,7 @@ class NetworkController extends GetxController {
   void _updateConnectionStatus(ConnectivityResult connectivityResult) async {
     if (connectivityResult == ConnectivityResult.none) {
       Get.dialog(
-        WillPopScope(child: Scaffold(
-          body: Container(
-            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 25),
-            color: AppColor.bgBlackColor,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  "assets/images/no_connection.svg",
-                  height: Get.height * 0.3,
-                ),
-                const Text(
-                  "No Connection",
-                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.w600, color: Colors.amber),
-                ),
-                const Text(
-                  "There is a connection error. Please check your internet and try again.",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 16),
-                ),
-                SizedBox(
-                  height: Get.height * 0.1,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      //This is just use to improve the UI. The dialog will be dismissed as soon as the connection is restored.
-                    },
-                    child: const Text(
-                      "Try Again",
-                      style: TextStyle(color: Colors.black),
-                    ))
-              ],
-            ),
-          ),
-        ), onWillPop:() async => false),
+        const NoConnectionDialog(),
           barrierDismissible: false);
     } else {
       bool isDeviceConnected = await InternetConnectionChecker().hasConnection;
