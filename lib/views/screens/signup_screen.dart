14e52072f75be9e3a286e8e3ae42a3290e1052f0
AKASHANGADII <akashakku2202@gmail.com>
2023-10-02 13:07:18 +0530

    fix : Cofirm Password Textfiled Persist Fix

--- lib/views/screens/signup_screen.dart
+++ lib/views/screens/signup_screen.dart
@@ -217,7 +217,7 @@ class _SignupScreenState extends State<SignupScreen> {
                       style: TextStyle(fontSize: UiSizes.size_14),
                     ),
                     GestureDetector(
-                      onTap: () => Get.offNamed(AppRoutes.login),
+                        onTap: () => Get.offNamed(AppRoutes.login),
                       child: Text(
                         "Login",
                         style: TextStyle(
