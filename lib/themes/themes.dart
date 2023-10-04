8e4f18ac32a45da342d8a47043fa1a7e185648d7
Vrund <vrundraval24@gmail.com>
2023-10-04 10:47:20 +0530

    refactor: changed background color to app_bg_color.

--- lib/themes/themes.dart
+++ lib/themes/themes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:resonate/utils/colors.dart';
 import 'package:resonate/utils/ui_sizes.dart';
 
 class Themes {
@@ -12,17 +13,18 @@ class Themes {
   );
 
   static final darkTheme = ThemeData(
-    scaffoldBackgroundColor: Colors.black,
+    scaffoldBackgroundColor: AppColor.bgBlackColor,
     colorScheme: const ColorScheme.dark(
         primary: Colors.amber,
         secondary: Colors.amber,
         onPrimary: Colors.black,
         onSecondary: Colors.black,
-        surface: Colors.black,
+        surface: AppColor.bgBlackColor,
         onSurface: Colors.amber),
     appBarTheme: AppBarTheme(
         foregroundColor: Colors.amber,
         toolbarHeight: UiSizes.size_56,
+        elevation: 0,
         titleTextStyle: const TextStyle(
             color: Colors.amber, fontWeight: FontWeight.w500, fontSize: 24)),
   );
