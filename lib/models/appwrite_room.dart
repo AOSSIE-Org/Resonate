ecec4bdf5487780655a26fa1b7e58fb44469714a
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-02-27 21:43:39 +0530

    complete the documentation of code files inside lib/models

--- lib/models/appwrite_room.dart
+++ lib/models/appwrite_room.dart
@@ -1,17 +1,21 @@
 import 'package:resonate/utils/enums/room_state.dart';
 
-class AppwriteRoom{
-  AppwriteRoom({
-    required this.id,
-    required this.name,
-    required this.description,
-    required this.totalParticipants,
-    required this.tags,
-    required this.memberAvatarUrls,
-    required this.state,
-    required this.isUserAdmin,
-    this.myDocId
-});
+//AppwriteRoom is a model class for an AppWrite room.
+//This class defines the structure of the Voice chat room.
+class AppwriteRoom {
+  AppwriteRoom(
+      {required this.id, //identifier for the room
+      required this.name, //name of the room
+      required this.description, //description or additional information about the room
+      required this.totalParticipants, //The total number of participants (users) in the room.
+      required this.tags, //A list of tags associated with the room
+      required this.memberAvatarUrls, //A list of URLs representing the avatars of members in the room
+      required this.state, //An object of type RoomState representing the state of the room.
+      required this.isUserAdmin, //A boolean indicating whether the user is an administrator of the room.
+      this.myDocId //optional parameter
+      });
+  //variables marked by `late final` will be initialized at a later point, and
+  //'final' indicates that once initialized, their values will not change.
   late final String id;
   late final String name;
   late final String description;
