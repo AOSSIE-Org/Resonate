59b01bc3447414b2f35bb22794bcd76a5fe07bf9
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-02-28 22:05:07 +0530

    document the codefiles for some widgets

--- lib/views/widgets/custom_card.dart
+++ lib/views/widgets/custom_card.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:resonate/utils/ui_sizes.dart';
 
+//CustomCard is a widget that creates a Card widget with modified properties
 class CustomCard extends StatelessWidget {
-  final String title;
-  final IconData icon;
-  final VoidCallback onTap;
+  final String title; //the title of the card
+  final IconData icon; //icon displayed on the card
+  final VoidCallback
+      onTap; //voidcallback onTap which will be executed when card is tapped
 
   const CustomCard(
       {super.key,
@@ -20,20 +22,27 @@ class CustomCard extends StatelessWidget {
       child: Card(
         margin: EdgeInsets.symmetric(
             vertical: UiSizes.height_10, horizontal: UiSizes.width_25),
+        //using InkWell to make card tappable and display a splash when user tapps
         child: InkWell(
+          //execute the VoidCallback onTap(from parameter list) when the card is tapped.
           onTap: onTap,
+          //Contents of the card
           child: Row(
             children: [
+              //Seperation
               SizedBox(
                 width: UiSizes.width_20,
               ),
+              //FaIcon is provided by font_awesome_flutter package.
               FaIcon(
                 icon,
                 size: UiSizes.size_24,
               ),
+              //Seperation
               SizedBox(
                 width: UiSizes.width_40,
               ),
+              //The title of the card
               Text(
                 title,
                 style: TextStyle(fontSize: UiSizes.size_14),
--- lib/views/widgets/profile_avatar.dart
+++ lib/views/widgets/profile_avatar.dart
@@ -1,55 +1,63 @@
+//import required packages
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:resonate/controllers/auth_state_controller.dart';
 import 'package:resonate/routes/app_routes.dart';
-import 'package:resonate/themes/theme_controller.dart';
 import 'package:resonate/utils/ui_sizes.dart';
 
+//profileAvatar is a clickable widget which navigates the user to profile route.
 Widget profileAvatar(BuildContext context) {
-  final ThemeController themeController = Get.find<ThemeController>();
-  final AuthStateController authStateController =
-      Get.put<AuthStateController>(AuthStateController());
-
   return GestureDetector(
     onTap: () => Navigator.pushNamed(context, AppRoutes.profile),
+    //contents of profile Avatar
     child: Padding(
-        padding: EdgeInsets.symmetric(
-            horizontal: UiSizes.width_10, vertical: UiSizes.height_10),
-        child: Obx(
-          () => Stack(
-            children: [
-              SizedBox(
-                width: UiSizes.width_35,
-                height: UiSizes.height_45,
-                child: CircularProgressIndicator(
-                  color: themeController.primaryColor.value,
-                  strokeWidth: UiSizes.width_2,
-                  value: 1,
-                ),
+      padding: EdgeInsets.symmetric(
+          horizontal: UiSizes.width_10, vertical: UiSizes.height_10),
+      //Using GetBuilder to track changes made by AuthStateController
+      //NOTE: since we are only concerned by changes of a single controller
+      //using GetBuilder than GetX or ObX is more economical.
+      child: GetBuilder<AuthStateController>(
+        builder: (controller) => Stack(
+          children: [
+            //Display a CircularProgressIndicator
+            //Since the value is 1(constant) the CircularProgressIndicator will not show any changes
+            SizedBox(
+              width: UiSizes.width_35,
+              height: UiSizes.height_45,
+              child: CircularProgressIndicator(
+                color: Colors.amber,
+                strokeWidth: UiSizes.width_2,
+                value: 1,
               ),
-              Positioned(
-                left: 0,
-                right: 0,
-                top: 0,
-                bottom: 0,
-                child: Center(
-                  child: CircleAvatar(
-                    backgroundColor: Colors.white,
-                    radius: UiSizes.size_20,
-                    onBackgroundImageError: (exception, stackTrace) =>
-                        const Icon(
-                      Icons.person_outline,
-                    ),
-                    backgroundImage: authStateController.profileImageUrl ==
-                                null ||
-                            authStateController.profileImageUrl!.isEmpty
-                        ? null
-                        : NetworkImage(authStateController.profileImageUrl!),
+            ),
+            //Use Positioned widget to position the child widget inside a Stack
+            Positioned(
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0,
+              child: Center(
+                //Use CircleAvatar to display a image
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: UiSizes.size_20,
+                  //if there are any errors or the image is still loading then display a person_outline icon.
+                  onBackgroundImageError: (exception, stackTrace) => const Icon(
+                    Icons.person_outline,
                   ),
+                  //use trenery operator to load image based on the specified condition
+                  //Condition: if the condition (profileImageUrl is null or empty) is true then backgroundImage is not shown
+                  //if the condition is false then image is loaded from profileImageUrl with the help of NetworkImage widget.
+                  backgroundImage: controller.profileImageUrl == null ||
+                          controller.profileImageUrl!.isEmpty
+                      ? null
+                      : NetworkImage(controller.profileImageUrl!),
                 ),
               ),
-            ],
-          ),
-        )),
+            ),
+          ],
+        ),
+      ),
+    ),
   );
 }
