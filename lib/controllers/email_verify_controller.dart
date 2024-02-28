8fe557a7c9d20fccd188335f3051c02e45d58fc1
Immanuel Dsouza <dsouzad681@gmail.com>
2024-02-28 22:23:09 +0530

    documented email-verify-controller.dart

--- lib/controllers/email_verify_controller.dart
+++ lib/controllers/email_verify_controller.dart
@@ -26,7 +26,8 @@ class EmailVerifyController extends GetxController {
   var isVerifying = false.obs;
   var isUpdateAllowed = true.obs;
   var signupisallowed = true.obs;
-  var clearTextField=false.obs;
+  var clearTextField = false.obs;
+
   AuthStateController authStateController = Get.find<AuthStateController>();
   AuthenticationController authController =
       Get.find<AuthenticationController>();
@@ -39,20 +40,31 @@ class EmailVerifyController extends GetxController {
     databases = Databases(authStateController.client);
   }
 
+  //This method provides the service of sending OTP to user
+  //by leveraging the AppWrite functions.
   Future<bool> sendOTP() async {
+    //sets the loading state to indicate ongoing process.
     authController.isLoading.value = true;
+
+    //Generates OTP by appending randomNumber with email.
     var otpID = randomNumeric(10).toString() + authStateController.email!;
+
     // Appwrite does not accept @ in document ID's
     otpID = otpID.split("@")[0];
+
     var sendOtpData = {
       "email": authStateController.email,
       "otpID": otpID.toString()
     };
 
+    //updates the user's document in Appwrite.
     await authStateController.account
         .updatePrefs(prefs: {"otp_ID": otpID, "isUserProfileComplete": true});
+
+    //Encodes the data to be sent to the function into a json string.
     var data = json.encode(sendOtpData);
 
+    //Triggers the Appwrite Function responsible for sending OTP emails.
     var res = await functions.createExecution(
         functionId: sendOtpFunctionID, data: data.toString());
 
@@ -75,6 +87,15 @@ class EmailVerifyController extends GetxController {
     return true;
   }
 
+  /*
+    This below method helps to verify the user entered otp
+    
+    Purpose of imp variables used in below code:
+    - otpId =>Retrieves the otp from user's document, representing the actual OTP.
+    - userOTP => The user entered OTP.
+    - verificationID => Based on the comparison , the function will determine whether user
+    entered the correct OTP.
+   */
   Future<void> verifyOTP(String userOTP) async {
     verificationID = randomNumeric(10).toString() + authStateController.email!;
     verificationID = verificationID.split("@")[0];
@@ -90,6 +111,7 @@ class EmailVerifyController extends GetxController {
         functionId: verifyOtpFunctionID, data: data.toString());
   }
 
+//Provides the verficiation Status of OTP entered by user.
   Future<String> checkVerificationStatus() async {
     final document = await databases.getDocument(
       databaseId: emailVerificationDatabaseID,
@@ -100,6 +122,7 @@ class EmailVerifyController extends GetxController {
     return isVerified;
   }
 
+  //This function makes the user Profile set Verified.
   Future<void> setVerified() async {
     var verifyUserData = {
       "userID": authStateController.uid,
@@ -109,6 +132,7 @@ class EmailVerifyController extends GetxController {
         functionId: verifyUserFunctionID, data: verifyData.toString());
   }
 
+  //Helps to update Email a creates a cloud function for it.
   Future<void> updateEmail() async {
     var updateEmailData = json.encode({
       "User_ID": authStateController.uid,
