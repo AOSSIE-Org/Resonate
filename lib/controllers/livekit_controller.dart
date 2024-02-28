96f2c214a8a80932309ae6fb6bc71abd528f7f02
Immanuel Dsouza <dsouzad681@gmail.com>
2024-02-28 22:42:36 +0530

    documented code in livekit_controller.dart

--- lib/controllers/livekit_controller.dart
+++ lib/controllers/livekit_controller.dart
@@ -5,7 +5,7 @@ import 'package:livekit_client/livekit_client.dart';
 import 'package:resonate/controllers/pair_chat_controller.dart';
 import 'package:resonate/controllers/single_room_controller.dart';
 
-class LiveKitController extends GetxController{
+class LiveKitController extends GetxController {
   late final Room liveKitRoom;
   late final EventsListener<RoomEvent> listener;
   final String liveKitUri;
@@ -13,14 +13,22 @@ class LiveKitController extends GetxController{
 
   LiveKitController({required this.liveKitUri, required this.roomToken});
 
+  /*
+    - In onInit() method, we try to create a new room and join it ,through roomtoken & liveKitURI.
+    - Setting up necessary listeners .
+   */
   @override
-  void onInit() async{
+  void onInit() async {
     await joinLiveKitRoom(liveKitUri: liveKitUri, roomToken: roomToken);
     liveKitRoom.addListener(onRoomDidUpdate);
     setUpListeners();
     super.onInit();
   }
 
+  /*
+    In the onClose method ,all the resources that were allocated before are released
+    to prevent resource exploitation and avoid memory leaks.
+   */
   @override
   void onClose() async {
     (() async {
@@ -31,10 +39,10 @@ class LiveKitController extends GetxController{
     super.onClose();
   }
 
-  Future<void> joinLiveKitRoom({required String liveKitUri, required String roomToken}) async {
+  Future<void> joinLiveKitRoom(
+      {required String liveKitUri, required String roomToken}) async {
     //
     try {
-
       //create new room
       liveKitRoom = Room();
 
@@ -53,7 +61,6 @@ class LiveKitController extends GetxController{
           ),
         ),
       );
-
     } catch (error) {
       log('Could not connect $error');
     }
@@ -63,23 +70,32 @@ class LiveKitController extends GetxController{
     //Callback which will be called on room update
   }
 
+  /*
+  - This below method's purpose is to set up event listeners to handle specific
+    events related to rooms, disconnections and recording status changes.
+   */
   void setUpListeners() => listener
     ..on<RoomDisconnectedEvent>((event) async {
       if (event.reason != null) {
         log('Room disconnected: reason => ${event.reason}');
       }
-      WidgetsBindingCompatible.instance
-          ?.addPostFrameCallback((timeStamp) {
-            if (Get.isRegistered<SingleRoomController>()){
-              Get.find<SingleRoomController>().leaveRoom();
-            }
-            else if (Get.isRegistered<PairChatController>()){
-              Get.find<PairChatController>().endChat();
-            }
+
+      /*
+      Schedules a callback to run after the current frame completes.
+      - Checks if Registered Controllers are :
+        1.If SingleRoomController  is registered , calls [leaveRoom()] to initate proper
+          room clean up.
+        2.If PairChatController is registered, calls [endChat()] to handle chat termination.
+       */
+      WidgetsBindingCompatible.instance?.addPostFrameCallback((timeStamp) {
+        if (Get.isRegistered<SingleRoomController>()) {
+          Get.find<SingleRoomController>().leaveRoom();
+        } else if (Get.isRegistered<PairChatController>()) {
+          Get.find<PairChatController>().endChat();
+        }
       });
     })
     ..on<RoomRecordingStatusChanged>((event) {
       //context.showRecordingStatusChangedDialog(event.activeRecording);
     });
-
 }
