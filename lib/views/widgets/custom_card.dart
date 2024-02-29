888660eb60231383dd4a86e9c95891679fc07530
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-02-29 12:45:00 +0530

    fix splash overflow issue

--- lib/views/widgets/custom_card.dart
+++ lib/views/widgets/custom_card.dart
@@ -17,28 +17,41 @@ class CustomCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: UiSizes.height_76,
-      child: Card(
+      //Use Container instead of Card
+      child: Container(
         margin: EdgeInsets.symmetric(
-            vertical: UiSizes.height_10, horizontal: UiSizes.width_25),
-        child: InkWell(
-          onTap: onTap,
-          child: Row(
-            children: [
-              SizedBox(
-                width: UiSizes.width_20,
-              ),
-              FaIcon(
-                icon,
-                size: UiSizes.size_24,
-              ),
-              SizedBox(
-                width: UiSizes.width_40,
-              ),
-              Text(
-                title,
-                style: TextStyle(fontSize: UiSizes.size_14),
-              )
-            ],
+          vertical: UiSizes.height_10,
+          horizontal: UiSizes.width_25,
+        ),
+        //Use Material to see splash effect of InkWell
+        child: Material(
+          color: const Color.fromARGB(255, 255, 250, 239),
+          //specify the border radius to create a rounded edge
+          borderRadius: BorderRadius.circular(15),
+          elevation: 1,
+          child: InkWell(
+            //adjust the borderRadius of inkwell to match with material widet
+            // to fix splash overlow issue.
+            borderRadius: BorderRadius.circular(15),
+            onTap: onTap,
+            child: Row(
+              children: [
+                SizedBox(
+                  width: UiSizes.width_20,
+                ),
+                FaIcon(
+                  icon,
+                  size: UiSizes.size_24,
+                ),
+                SizedBox(
+                  width: UiSizes.width_40,
+                ),
+                Text(
+                  title,
+                  style: TextStyle(fontSize: UiSizes.size_14),
+                )
+              ],
+            ),
           ),
         ),
       ),
