1591c3f65369637df28e0949826fa8dcad1435a8
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-03-01 07:45:10 +0530

    complete documenting codefiles inside lib/services

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,3 +1,4 @@
+//import required packages
 import 'dart:convert';
 import 'dart:developer';
 
@@ -5,25 +6,29 @@ import 'package:appwrite/appwrite.dart';
 import 'package:resonate/services/appwrite_service.dart';
 import 'package:resonate/utils/constants.dart';
 
+//ApiService class is used to communicate with AppWrite
 class ApiService {
-
+  //Functions is provided by appwrite package
+  //functions now holds the AppWrite client obtained from getClient() function defined in AppwriteService(lib\services\appwrite_service.dart)
   final Functions functions = Functions(AppwriteService.getClient());
-
-  Future<Map<String, dynamic>> createRoom(
-      String roomName, String roomDescription, String adminUid, List<String> roomTags) async {
+  //create room function defines a data map containing information about the room to be created
+  Future<Map<String, dynamic>> createRoom(String roomName,
+      String roomDescription, String adminUid, List<String> roomTags) async {
     final data = {
       "name": roomName,
       "description": roomDescription,
       "adminUid": adminUid,
       "tags": roomTags,
     };
-
+    //decode the data obtained from createExecution() method and return a data map called responseData
+    //createExecution is one of the methods defined in Functions class provided by appwrite package.
     try {
-      final response = await functions.createExecution(functionId: createRoomServiceId, data: json.encode(data));
+      final response = await functions.createExecution(
+          functionId: createRoomServiceId, data: json.encode(data));
 
       if (response.statusCode == 200) {
         log(response.response);
-        
+
         final Map<String, dynamic> responseData = jsonDecode(response.response);
 
         return responseData;
@@ -35,6 +40,7 @@ class ApiService {
     }
   }
 
+  //joinRoom method has information about
   Future<Map<String, dynamic>> joinRoom(String roomName, String uid) async {
     final data = {
       "roomName": roomName,
@@ -42,7 +48,8 @@ class ApiService {
     };
 
     try {
-      final response = await functions.createExecution(functionId: joinRoomServiceId, data: json.encode(data));
+      final response = await functions.createExecution(
+          functionId: joinRoomServiceId, data: json.encode(data));
 
       if (response.statusCode == 200) {
         log(response.response);
@@ -58,14 +65,17 @@ class ApiService {
     }
   }
 
-  Future<Map<String, dynamic>> deleteRoom(String appwriteRoomDocId, String token) async {
+  //deleteRoom is used to delete the room from the Appwrite db.
+  Future<Map<String, dynamic>> deleteRoom(
+      String appwriteRoomDocId, String token) async {
     final data = {
       "appwriteRoomDocId": appwriteRoomDocId,
       "token": token,
     };
 
     try {
-      final response = await functions.createExecution(functionId: deleteRoomServiceId, data: json.encode(data));
+      final response = await functions.createExecution(
+          functionId: deleteRoomServiceId, data: json.encode(data));
 
       if (response.statusCode == 200) {
         log(response.response);
