94b1f582f428a9962fa6b6b985e507247b2f682f
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-03-04 22:05:59 +0530

    complete documenting create_room_controller.dart and create_room_controller.dart

--- lib/controllers/create_room_controller.dart
+++ lib/controllers/create_room_controller.dart
@@ -1,3 +1,4 @@
+//import required packages
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
@@ -5,22 +6,26 @@ import 'package:get/get.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:resonate/controllers/auth_state_controller.dart';
 import 'package:resonate/controllers/tabview_controller.dart';
-import 'package:resonate/themes/theme_controller.dart';
 import 'package:resonate/utils/enums/room_state.dart';
 import 'package:textfield_tags/textfield_tags.dart';
 
 import '../models/appwrite_room.dart';
 import '../services/room_service.dart';
 
+// CreateRoomController handles the creation of rooms
 class CreateRoomController extends GetxController {
-  final ThemeController themeController = Get.find<ThemeController>();
-  RxBool isLoading = false.obs;
-  RxBool isScheduled = false.obs;
-  GlobalKey<FormState> createRoomFormKey = GlobalKey<FormState>();
+  RxBool isLoading = false
+      .obs; //reactive variable for checking the state of CreateRoomController
+  RxBool isScheduled = false
+      .obs; //reactive variable for check weather the room creation is scheduled
+  GlobalKey<FormState> createRoomFormKey = GlobalKey<
+      FormState>(); //Global Key for managing the form state. Usually used for validating the text forms
+  //controllers for managing various text fields
   TextEditingController nameController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
   TextfieldTagsController tagsController = TextfieldTagsController();
 
+  //dispose the TextEditingControllers as soon as the CreateRoomController goes out of memory
   @override
   void dispose() {
     nameController.dispose();
@@ -29,29 +34,33 @@ class CreateRoomController extends GetxController {
     super.dispose();
   }
 
+  //method for creating a room. It takes name, description, tags and fromCreateScreen as parameters
   Future<void> createRoom(String name, String description, List<String> tags,
       bool fromCreateScreen) async {
     if (fromCreateScreen) {
       if (!createRoomFormKey.currentState!.validate()) {
+        //validate the form if fromCreateScreen is true
         return;
       }
     }
 
     try {
+      //mark CreateRoomController's isLoading reactive var as true
       isLoading.value = true;
 
       // Display Loading Dialog
       Get.dialog(
           Center(
             child: LoadingAnimationWidget.threeRotatingDots(
-                color: themeController.primaryColor.value,
-                size: Get.pixelRatio * 20),
+                color: Colors.amber, size: Get.pixelRatio * 20),
           ),
           barrierDismissible: false,
           name: "Loading Dialog");
 
       // Create a new room and add current user to participant list as admin and join livekit room
       AuthStateController authStateController = Get.find<AuthStateController>();
+      //RoomService class is defined in lib\services\room_service.dart
+      //It provides various methods for handling Rooms
       List<String> newRoomInfo = await RoomService.createRoom(
           roomName: name,
           roomDescription: description,
