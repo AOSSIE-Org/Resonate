1e778435a9b09da8a65b6a0b2eff85154cd2e2f6
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-03-12 12:57:01 +0530

    dialog widgetRefactor no connection

--- lib/views/widgets/no_connection_dialog.dart
+++ lib/views/widgets/no_connection_dialog.dart
@@ -12,46 +12,54 @@ class NoConnectionDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-        child: Scaffold(
-          body: Center(
-            child: SingleChildScrollView(
-              child: Container(
-                padding: EdgeInsets.symmetric(
-                    vertical: UiSizes.height_24_6,
-                    horizontal: UiSizes.width_25),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SvgPicture.asset(
-                      AppImages.noConnectionImage,
-                      height: UiSizes.height_246,
-                    ),
-                    Text(
-                      "No Connection",
+      child: Scaffold(
+        body: Center(
+          //SingleChildScrollView allows the page to be made scrollable(vertical scroll by default)
+          child: SingleChildScrollView(
+            //Contents of the page
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                  vertical: UiSizes.height_24_6, horizontal: UiSizes.width_25),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //display a svg from the specified path
+                  SvgPicture.asset(
+                    //path to images is specified in AppImages which can be found in lib\utils\app_images.dart
+                    AppImages.noConnectionImage,
+                    height: UiSizes.height_246,
+                  ),
+                  //Simple text to display that the user is not connected
+                  Text(
+                    "No Connection",
+                    style: TextStyle(
+                        fontSize: UiSizes.size_40,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.amber),
+                  ),
+                  Text(
+                    "There is a connection error. Please check your internet and try again.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: UiSizes.size_16),
+                  ),
+                  //Seperation
+                  SizedBox(
+                    height: UiSizes.height_82,
+                  ),
+                  //an elevatd button which displays "Try Again"
+                  ElevatedButton(
+                    //when user presses the button Get looks for NetworkController and
+                    //calls tryAgain() method specified in NetworkController
+                    onPressed: () {
+                      Get.find<NetworkController>().tryAgain();
+                    },
+                    child: Text(
+                      "Try Again",
                       style: TextStyle(
-                          fontSize: UiSizes.size_40,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.amber),
+                          color: Colors.black, fontSize: UiSizes.size_14),
                     ),
-                    Text(
-                      "There is a connection error. Please check your internet and try again.",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: UiSizes.size_16),
-                    ),
-                    SizedBox(
-                      height: UiSizes.height_82,
-                    ),
-                    ElevatedButton(
-                        onPressed: () {
-                          Get.find<NetworkController>().tryAgain();
-                        },
-                        child: Text(
-                          "Try Again",
-                          style: TextStyle(
-                              color: Colors.black, fontSize: UiSizes.size_14),
-                        ))
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
