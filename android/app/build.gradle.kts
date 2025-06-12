bc902580659501d2fa0cc8f793d67af49899b3b2
Madhav Gupta <madhav.gupta7674@gmail.com>
2025-06-12 19:23:09 +0530

    feat: Upgrade Android config from Groovy to Kotlin DSL

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,56 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    id("com.android.application")
+    id("com.google.gms.google-services")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace ="com.resonate.resonate"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.resonate.resonate"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://docs.flutter.dev/deployment/android#reviewing-the-build-configuration.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode.toInt()
+        versionName = flutter.versionName
+        manifestPlaceholders += mapOf("auth0Domain" to "dev-5w4x3qxvszw8f0u6.us.auth0.com", "auth0Scheme" to "resonate")
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.22")
+     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.0.9")
+     implementation("androidx.window:window:1.0.0")
+     implementation("androidx.window:window-java:1.0.0")
+}
