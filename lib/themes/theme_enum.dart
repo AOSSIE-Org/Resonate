a7a10d1b39a81c817f47db2abc0f0e0a543696d3
TheGoodUser <ram19870101@gmail.com>
2025-09-12 01:04:55 +0530

    feat(theme-selection): improve UI and smooth UX for theme choices

--- lib/themes/theme_enum.dart
+++ lib/themes/theme_enum.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+
 enum Themes { classic, vintage, forest, cream, amber, time }
+
+// Icons w.r.t their Themes
+enum ThemeIcons {
+  classic('classic', Icons.diamond),
+  vintage('vintage', Icons.temple_buddhist_rounded),
+  forest('forest', Icons.forest),
+  cream('cream', Icons.nights_stay),
+  amber('amber', Icons.local_fire_department),
+  time('time', Icons.access_time_outlined);
+
+  final String theme;
+  final IconData icon;
+  const ThemeIcons(this.theme, this.icon);
+}
+  
