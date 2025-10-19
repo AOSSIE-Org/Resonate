58e9d35eef24cefcc659974caa24074d500377cd
janvi <janvi220228@gmail.com>
2025-10-19 11:31:06 +0530

    feat: add Marathi translation support

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -8,6 +8,7 @@ import 'package:intl/intl.dart' as intl;
 import 'app_localizations_en.dart';
 import 'app_localizations_gu.dart';
 import 'app_localizations_hi.dart';
+import 'app_localizations_mr.dart';
 
 // ignore_for_file: type=lint
 
@@ -95,6 +96,7 @@ abstract class AppLocalizations {
 
   /// A list of this localizations delegate's supported locales.
   static const List<Locale> supportedLocales = <Locale>[
+    Locale('mr'),
     Locale('en'),
     Locale('gu'),
     Locale('hi'),
@@ -2288,7 +2290,7 @@ class _AppLocalizationsDelegate
 
   @override
   bool isSupported(Locale locale) =>
-      <String>['en', 'gu', 'hi'].contains(locale.languageCode);
+      <String>['mr', 'en', 'gu', 'hi'].contains(locale.languageCode);
 
   @override
   bool shouldReload(_AppLocalizationsDelegate old) => false;
@@ -2297,6 +2299,8 @@ class _AppLocalizationsDelegate
 AppLocalizations lookupAppLocalizations(Locale locale) {
   // Lookup logic when only language code is specified.
   switch (locale.languageCode) {
+    case 'mr':
+      return AppLocalizationsMr();
     case 'en':
       return AppLocalizationsEn();
     case 'gu':
