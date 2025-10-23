f366e1e9858d738da553abad28c96406bcc5e47a
uju09 <24bcs012@iiitdwd.ac.in>
2025-10-23 19:17:24 +0530

    Restored build.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,73 +1,21 @@
-import java.util.Properties
-import java.io.FileInputStream
-
-plugins {
-    id("com.android.application")
-    id("com.google.gms.google-services")
-    id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
-val keystoreProperties = Properties()
-val keystorePropertiesFile = rootProject.file("key.properties")
-if (keystorePropertiesFile.exists()) {
-    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-}
-
-android {
-    namespace = "com.resonate.resonate"
-    compileSdk = 36
-    ndkVersion = "28.0.12433566"
-
-    compileOptions {
-        isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
-    }
-
-    kotlinOptions {
-        jvmTarget = "21"
-    }
-
-    val projectId: String = System.getenv("APPWRITE_PROJECT_ID") ?: "resonate"
-    println("PROJECT_ID: $projectId")
-    
-    defaultConfig {
-        applicationId = "com.resonate.resonate"
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode.toInt()
-        versionName = flutter.versionName
-        manifestPlaceholders += mapOf(
-            "auth0Domain" to "dev-5w4x3qxvszw8f0u6.us.auth0.com", 
-            "auth0Scheme" to "resonate", 
-            "PROJECT_ID" to projectId
-        )
-    }
-
-    signingConfigs {
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
-        }
-    }
-
-    buildTypes {
-        release {
-            // Use release signing config for production builds
-            signingConfig = signingConfigs.getByName("release")
-        }
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
 }
 
-flutter {
-    source = "../.."
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
 }
 
-dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
-    implementation("androidx.window:window:1.0.0")
-    implementation("androidx.window:window-java:1.0.0")
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
