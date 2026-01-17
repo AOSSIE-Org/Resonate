49d626f4ccc6b313ae36afb3b966cdde2e1399a5
night007-hub <m.saiadithya2006@gmail.com>
2026-01-17 21:17:27 +0530

    feat: zen sleep timer implementation with Android permissions

--- lib/controllers/chapter_player_controller.dart
+++ lib/controllers/chapter_player_controller.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:get/get.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter_lyric/lyrics_reader_model.dart';
@@ -11,6 +12,11 @@ class ChapterPlayerController extends GetxController {
   late Duration chapterDuration;
   late LyricsReaderModel lyricModel;
 
+
+
+  Timer? _sleepTimer;
+  Rxn<int> sleepTimerRemaining = Rxn<int>();
+
   void initialize(
     AudioPlayer player,
     LyricsReaderModel model,
@@ -20,6 +26,8 @@ class ChapterPlayerController extends GetxController {
     lyricModel = model;
     chapterDuration = duration;
     audioPlayer?.setReleaseMode(ReleaseMode.stop);
+
+
 
     audioPlayer?.onPositionChanged.listen((Duration event) {
       sliderProgress.value = event.inMilliseconds.toDouble();
@@ -34,14 +42,49 @@ class ChapterPlayerController extends GetxController {
   void togglePlayPause() {
     if (isPlaying.value) {
       audioPlayer?.pause();
+      audioPlayer?.setVolume(1.0); 
     } else {
+      audioPlayer?.setVolume(1.0);
       audioPlayer?.resume();
     }
     isPlaying.value = !isPlaying.value;
   }
 
+
+
+  void startSleepTimer(int minutes) {
+    cancelSleepTimer();
+    sleepTimerRemaining.value = minutes * 60;
+    
+    _sleepTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (sleepTimerRemaining.value != null && sleepTimerRemaining.value! > 0) {
+        sleepTimerRemaining.value = sleepTimerRemaining.value! - 1;
+        
+        if (sleepTimerRemaining.value! <= 60 && isPlaying.value) {
+          double vol = sleepTimerRemaining.value! / 60.0;
+          if (vol < 0) vol = 0;
+          audioPlayer?.setVolume(vol);
+        }
+      } else {
+        audioPlayer?.pause();
+        audioPlayer?.setVolume(1.0);
+        isPlaying.value = false;
+        cancelSleepTimer();
+      }
+    });
+  }
+
+  void cancelSleepTimer() {
+    _sleepTimer?.cancel();
+    _sleepTimer = null;
+    sleepTimerRemaining.value = null;
+    sleepTimerRemaining.value = null;
+    audioPlayer?.setVolume(1.0);
+  }
+
   @override
   void onClose() {
+    _sleepTimer?.cancel();
     audioPlayer?.release();
     super.onClose();
   }
