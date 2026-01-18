67228eb2e6aa48a23305feff1fec8005c448e452
kumbharkajal <kumbharkajal577@gmail.com>
2026-01-18 12:10:49 +0530

    fix(i18n): regenerate l10n after Marathi fix

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -1467,7 +1467,7 @@ abstract class AppLocalizations {
   /// Error message when a chosen username is too short.
   ///
   /// In en, this message translates to:
-  /// **'Username should contain more than 5 characters.'**
+  /// **'Username should contain more than 7 characters.'**
   String get usernameCharacterLimit;
 
   /// Generic button text for submitting a form.
@@ -2518,17 +2518,29 @@ abstract class AppLocalizations {
   /// **'Are you sure you want to delete this message?'**
   String get deleteMessageContent;
 
-  /// No description provided for @thisMessageWasDeleted.
+  /// Status text shown when a previously sent message has been deleted.
   ///
   /// In en, this message translates to:
   /// **'This message was deleted'**
   String get thisMessageWasDeleted;
 
-  /// No description provided for @failedToDeleteMessage.
+  /// Error message shown when the system is unable to delete a message.
   ///
   /// In en, this message translates to:
   /// **'Failed to delete message'**
   String get failedToDeleteMessage;
+
+  /// Validation error displayed when the user enters a username with unsupported characters.
+  ///
+  /// In en, this message translates to:
+  /// **'Please enter a valid username. Only letters, numbers, dots, underscores, and hyphens are allowed.'**
+  String get usernameInvalidFormat;
+
+  /// Error shown when the chosen username is unavailable because another user has already registered it.
+  ///
+  /// In en, this message translates to:
+  /// **'This username is already taken. Try a different one.'**
+  String get usernameAlreadyTaken;
 }
 
 class _AppLocalizationsDelegate
