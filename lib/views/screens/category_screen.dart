b4652bb553a1b5feedc7209dfd390efba7c9439d
Mahmoud <mahmoudsayeedr80@gmail.com>
2026-03-26 06:33:14 +0200

    Make Category Screen responsive

--- lib/views/screens/category_screen.dart
+++ lib/views/screens/category_screen.dart
@@ -20,65 +20,63 @@ class CategoryScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: true,//back button for user experience
+        automaticallyImplyLeading: true, //back button for user experience
         centerTitle: true,
         backgroundColor: Theme.of(context).colorScheme.surface,
         elevation: 0,
         title: Text(capitalizeFirstLetter(categoryName)),
       ),
       body: Obx(
-            () => exploreStoryController.isLoadingCategoryPage.value
+        () => exploreStoryController.isLoadingCategoryPage.value
             ? Center(
-          child: SizedBox(
-            height: 200.h,
-            width: 200.w,
-            child: LoadingIndicator(
-              indicatorType: Indicator.ballRotate,
-              colors: [Theme.of(context).colorScheme.primary],
-            ),
-          ),
-        )
+                child: SizedBox(
+                  height: 200.h,
+                  width: 200.w,
+                  child: LoadingIndicator(
+                    indicatorType: Indicator.ballRotate,
+                    colors: [Theme.of(context).colorScheme.primary],
+                  ),
+                ),
+              )
             : exploreStoryController.openedCategotyStories.isNotEmpty
             ? Padding(
-          padding:  EdgeInsets.only(top: 20.0.h),
-          child: ListView.builder(
-            scrollDirection: Axis.vertical,
-            padding: EdgeInsets.zero,
-            itemCount:
-            exploreStoryController.openedCategotyStories.length,
-            itemBuilder: (context, index) {
-              final int storyIndex = index;
-              return StoryListTile(
-                story: exploreStoryController
-                    .openedCategotyStories[storyIndex],
-              );
-            },
-          ),
-        )
-            : Padding(
-          padding:  EdgeInsets.only(bottom: 150.0.h),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,// take the least size needed
-            children: [
-              Image.asset(
-                height: 200.h,
-                width: 200.w,
-                AppImages.emptyBoxImage,
-              ),
-               SizedBox(height: 20.h),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 30.0.w),
-                child: Text(
-                  AppLocalizations.of(context)!.noStoriesInCategory(
-                    capitalizeFirstLetter(categoryName),
+                padding: EdgeInsets.only(top: 20.0.h),
+                child: ListView.builder(
+                  padding: EdgeInsets.zero,
+                  itemCount:
+                      exploreStoryController.openedCategotyStories.length,
+                  itemBuilder: (context, index) {
+                    final int storyIndex = index;
+                    return StoryListTile(
+                      story: exploreStoryController
+                          .openedCategotyStories[storyIndex],
+                    );
+                  },
+                ),
+              )
+            : Center(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30.0.w),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.asset(
+                        height: 200.h,
+                        width: 200.w,
+                        AppImages.emptyBoxImage,
+                      ),
+                      SizedBox(height: 20.h),
+                      Text(
+                        AppLocalizations.of(context)!.noStoriesInCategory(
+                          capitalizeFirstLetter(categoryName),
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
                   ),
-                  textAlign: TextAlign.center,
                 ),
               ),
-            ],
-          ),
-        ),
       ),
     );
   }
